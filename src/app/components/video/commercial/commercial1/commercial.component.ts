83ce51f1bc7950c69af87c2f9f655caa9807739d
Gaston Gaitan <80048998+Gaston1905@users.noreply.github.com>
2023-02-06 14:24:12 -0300

    change colour

--- src/app/components/video/commercial/commercial1/commercial.component.ts
+++ src/app/components/video/commercial/commercial1/commercial.component.ts
@@ -1,25 +1,18 @@
 import {
-  AfterViewInit,
   ChangeDetectorRef,
   Component,
   ElementRef,
-  OnDestroy,
   OnInit,
   ViewChild,
 } from '@angular/core';
 
 @Component({
   template:
-    '<div class="video-container" style="display: flex; justify-content: center;"><iframe class="youtube-video rounded rounded-4" frameborder="0" allowfullscreen="1" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" title="Baby Shark Dance | #babyshark Most Viewed Video | Animal Songs | PINKFONG Songs for Children" src="https://www.youtube.com/embed/2fvDFJKzhAk?enablejsapi=1&amp;origin=http://localhost:4200/home;widgetid=1" sandbox="allow-same-origin allow-scripts allow-forms allow-popups allow-popups-to-escape-sandbox" ></iframe></div>',
+    '<iframe class="youtube-video rounded rounded-4" frameborder="0" allowfullscreen="1" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" title="Baby Shark Dance | #babyshark Most Viewed Video | Animal Songs | PINKFONG Songs for Children" width="550" height="350" src="https://www.youtube.com/embed/2fvDFJKzhAk?enablejsapi=1&amp;origin=http://localhost:4200/home;widgetid=1" sandbox="allow-same-origin allow-scripts allow-forms allow-popups allow-popups-to-escape-sandbox" ></iframe>',
   selector: 'app-commercial',
 })
-export class CommercialComponent implements OnInit, AfterViewInit, OnDestroy {
-  @ViewChild('youTubePlayer') youTubePlayer!: ElementRef<HTMLDivElement>;
-  videoWidth: number | undefined;
-  videoHeight: number | undefined;
+export class CommercialComponent implements OnInit {
   private apiLoaded: boolean = false;
-
-  constructor(private _changeDetectorRef: ChangeDetectorRef) {}
 
   ngOnInit(): void {
     if (!this.apiLoaded) {
@@ -29,24 +22,5 @@ export class CommercialComponent implements OnInit, AfterViewInit, OnDestroy {
       document.body.appendChild(tag);
     }
     this.apiLoaded = true;
-  }
-
-  ngAfterViewInit(): void {
-    this.onResize();
-    window.addEventListener('resize', this.onResize);
-  }
-
-  onResize = (): void => {
-    // Automatically expand the video to fit the page up to 1200px x 720px
-    this.videoWidth = Math.min(
-      this.youTubePlayer.nativeElement.clientWidth,
-      1200
-    );
-    this.videoHeight = this.videoWidth * 0.6;
-    this._changeDetectorRef.detectChanges();
-  };
-
-  ngOnDestroy(): void {
-    window.removeEventListener('resize', this.onResize);
   }
 }
