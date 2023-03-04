aa2b0750e07b1ccd2fa0fbda839938462f438649
Gaston Gaitan <80048998+Gaston1905@users.noreply.github.com>
2023-03-04 03:21:22 -0300

    implement responsive video and change visual

--- src/app/components/video/commercial/commercial1/commercial.component.ts
+++ src/app/components/video/commercial/commercial1/commercial.component.ts
@@ -7,8 +7,7 @@ import {
 } from '@angular/core';
 
 @Component({
-  template:
-    '<iframe class="youtube-video rounded rounded-4" frameborder="0" allowfullscreen="1" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" title="Baby Shark Dance | #babyshark Most Viewed Video | Animal Songs | PINKFONG Songs for Children" width="550" height="350" src="https://www.youtube.com/embed/2fvDFJKzhAk?enablejsapi=1&amp;origin=http://localhost:4200/home;widgetid=1" sandbox="allow-same-origin allow-scripts allow-forms allow-popups allow-popups-to-escape-sandbox" ></iframe>',
+  template: './commercial.component.html',
   selector: 'app-commercial',
 })
 export class CommercialComponent implements OnInit {
