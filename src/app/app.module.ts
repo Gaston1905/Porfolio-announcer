c06264661aeb897b6e73c7090c2063134c3148ff
Gaston Gaitan <gaston.gaitan04@gmail.com>
2023-05-14 07:26:23 -0300

    update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,7 +6,6 @@ import { SharedModule } from './shared/shared.module';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
-import { AngularSvgIconModule } from 'angular-svg-icon';
 
 @NgModule({
   declarations: [AppComponent],
