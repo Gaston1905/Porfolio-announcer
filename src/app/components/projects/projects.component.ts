be90016caffb2fcb604a864cd0f48dae64786b98
Gaston Gaitan <gaston.gaitan04@gmail.com>
2023-05-15 04:07:12 -0300

    update

--- src/app/components/projects/projects.component.ts
+++ src/app/components/projects/projects.component.ts
@@ -7,9 +7,46 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProjectsComponent implements OnInit {
 
+    // Variable para almacenar los observadores
+    youtubeObservers: IntersectionObserver[] = [];
+
   constructor() { }
 
   ngOnInit(): void {
+        // Llama a la función para iniciar los observadores de intersección en el evento 'DOMContentLoaded'
+        this.initializeYouTubeObservers();
+  }
+
+
+  // Función para cargar el embebido de YouTube cuando sea visible
+  loadYouTubeEmbed(embedElement: HTMLIFrameElement) {
+    const videoSrc = embedElement.getAttribute('data-src');
+    if (videoSrc) {
+      embedElement.setAttribute('src', videoSrc);
+    }
+  }
+
+  // Función de devolución de llamada para el observador de intersección
+  intersectionCallback(entries: IntersectionObserverEntry[], observer: IntersectionObserver) {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        const embedElement = entry.target as HTMLIFrameElement;
+        this.loadYouTubeEmbed(embedElement);
+        observer.unobserve(embedElement);
+      }
+    });
+  }
+
+  // Función para iniciar los observadores de intersección en los elementos embebidos de YouTube
+  initializeYouTubeObservers() {
+    const embedElements = document.querySelectorAll('iframe[data-src]');
+    embedElements.forEach((embedElement) => {
+      const observer = new IntersectionObserver(this.intersectionCallback.bind(this), { threshold: 0.1 });
+      console.log(observer)
+      observer.observe(embedElement);
+      console.log(embedElement)
+      this.youtubeObservers.push(observer);
+    });
   }
 
 }
