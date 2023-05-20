e5fcf8e7c3d7949e12badea3513799319846a96d
Gaston Gaitan <80048998+Gaston1905@users.noreply.github.com>
2023-05-20 14:52:06 -0300

    update

--- src/app/components/projects/projects.component.ts
+++ src/app/components/projects/projects.component.ts
@@ -3,26 +3,18 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-projects',
   templateUrl: './projects.component.html',
-  styleUrls: ['./projects.component.scss']
+  styleUrls: ['./projects.component.scss'],
 })
 export class ProjectsComponent implements OnInit {
+  // Variable para almacenar los observadores
+  youtubeObservers: IntersectionObserver[] = [];
 
-  isPaused: boolean = false;
-  isDimmed: boolean = false;
-  isDimming: boolean = false;
-  isPlay: boolean = false;
-  isClickedPlay: boolean = false;
-
-    // Variable para almacenar los observadores
-    youtubeObservers: IntersectionObserver[] = [];
-
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {
-        // Llama a la función para iniciar los observadores de intersección en el evento 'DOMContentLoaded'
-        this.initializeYouTubeObservers();
+    // Llama a la función para iniciar los observadores de intersección en el evento 'DOMContentLoaded'
+    this.initializeYouTubeObservers();
   }
-
 
   // Función para cargar el embebido de YouTube cuando sea visible
   loadYouTubeEmbed(embedElement: HTMLIFrameElement) {
@@ -33,7 +25,10 @@ export class ProjectsComponent implements OnInit {
   }
 
   // Función de devolución de llamada para el observador de intersección
-  intersectionCallback(entries: IntersectionObserverEntry[], observer: IntersectionObserver) {
+  intersectionCallback(
+    entries: IntersectionObserverEntry[],
+    observer: IntersectionObserver
+  ) {
     entries.forEach((entry) => {
       if (entry.isIntersecting) {
         const embedElement = entry.target as HTMLIFrameElement;
@@ -47,20 +42,14 @@ export class ProjectsComponent implements OnInit {
   initializeYouTubeObservers() {
     const embedElements = document.querySelectorAll('iframe[data-src]');
     embedElements.forEach((embedElement) => {
-      const observer = new IntersectionObserver(this.intersectionCallback.bind(this), { threshold: 0.1 });
-      console.log(observer)
+      const observer = new IntersectionObserver(
+        this.intersectionCallback.bind(this),
+        { threshold: 0.1 }
+      );
+      console.log(observer);
       observer.observe(embedElement);
-      console.log(embedElement)
+      console.log(embedElement);
       this.youtubeObservers.push(observer);
     });
   }
-
-  loadBackground() {
-    this.isDimmed = true;
-    this.isPlay = true;
-    this.isPaused = false;
-    this.isDimming = false; // Asegúrate de desactivar el dimming cuando se presiona play
-    console.log('me ejecuto')
-  }
-
 }
